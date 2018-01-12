4e8ee37449ef7b2988f0a21eb21f997415a48408
Sabinico <sabinico@gmail.com>
2018-01-12 18:49:42 +0100

    Fix category and permalink

--- admin/partials/wp-yoast-export-admin-table.php
+++ admin/partials/wp-yoast-export-admin-table.php
@@ -44,8 +44,8 @@
         <td><?php print $post->post_date; ?></td>
         <td><?php print $post->post_title; ?></td>
         <td><?php print get_user_by('ID',$post->post_author)->display_name; ?></td>
-        <td><?php wp_get_post_categories($post->ID); ?></td>
-        <td><?php print $post->guid; ?></td>
+        <td><?php print implode(", ", get_the_category($post->ID)); ?></td>
+        <td><?php print get_permalink($post->guid); ?></td>
         <?php if($options['export_content']): ?>
         <td><?php print $post->content_plain; ?></td>
         <?php endif; ?>
